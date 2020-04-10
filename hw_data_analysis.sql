736fe7ad90057464f6f92164e6d7e4938110f62e
gfisherjr <60365739+gfisherjr@users.noreply.github.com>
2020-04-10 16:21:13 -0400

    Update hw_data_analysis.sql

--- hw_data_analysis.sql
+++ hw_data_analysis.sql
@@ -48,3 +48,45 @@ INNER JOIN data_employees on data_employees.emp_no = dept_emp.emp_no
 WHERE dept_emp.to_date = '9999-01-01'
 */
 ORDER BY "emp_no";
+
+--List all employees whose first name is "Hercules" and last names begin with "B."
+SELECT data_employees.emp_no,
+	data_employees.first_name,
+	data_employees.last_name
+FROM data_employees
+WHERE first_name = 'Hercules'
+AND last_name LIKE 'B%';
+
+--List all employees in the Sales department, including their employee number, last name, first name, and department name.
+SELECT dept_emp.emp_no,
+	data_employees.last_name,
+	data_employees.first_name,
+	departments.dept_name
+FROM dept_emp
+INNER JOIN data_employees ON data_employees.emp_no = dept_emp.emp_no
+INNER JOIN departments ON departments.dept_no = dept_emp.dept_no
+WHERE departments.dept_no = 'd007'
+ORDER BY "emp_no";
+
+--List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+SELECT dept_emp.emp_no,
+	data_employees.last_name,
+	data_employees.first_name,
+	departments.dept_name
+FROM dept_emp
+INNER JOIN data_employees ON data_employees.emp_no = dept_emp.emp_no
+INNER JOIN departments ON departments.dept_no = dept_emp.dept_no
+WHERE departments.dept_no = 'd007'
+OR departments.dept_no = 'd005'
+ORDER BY "emp_no";
+
+--In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+SELECT last_name, COUNT(last_name) AS last_name_count
+FROM data_employees
+GROUP BY last_name
+ORDER BY last_name_count;
+
+SELECT * FROM data_employees
+WHERE emp_no = 499942;
+
+
